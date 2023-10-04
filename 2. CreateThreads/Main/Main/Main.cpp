dc8dcbee0553300bbe280ab84256c6156bacac43
Trinper <frolov24082005@gmail.com>
2023-10-04 13:14:52 +0300

    Add solution

--- 2. CreateThreads/Main/Main/Main.cpp
+++ 2. CreateThreads/Main/Main/Main.cpp
@@ -9,11 +9,11 @@ int* arr = nullptr;
 int Max = INT_MIN;
 int Min = INT_MAX;
 int sumEl = 0;
-double half = 0;
+int half = 0;
 
 CRITICAL_SECTION cs;
 
-DWORD WINAPI extremum(LPVOID) {
+DWORD WINAPI min_max(LPVOID) {
 	for (int i = 0; i < n; i++) {
 		Max = max(Max, arr[i]);
 		Sleep(10);
@@ -33,19 +33,55 @@ DWORD WINAPI average(LPVOID) {
 		sumEl += arr[i];
 		Sleep(10);
 	}
-	half = (double)sumEl / n;
+	half = sumEl / n;
 	EnterCriticalSection(&cs);
 	std::cout << "Average:" << half << "\n";
 	LeaveCriticalSection(&cs);
 
 	return 0;
 }
+void cinArr() {
+	std::cout << "N=: ";
+	std::cin >> n;
 
+	arr = new int[n];
+	std::cout << "Elements of array: " << '\n';
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
+}
+void swapExtremum() {
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == Max || arr[i] == Min) {
+			arr[i] = half;
+		}
+	}
+}
+
+void coutArr() {
+	std::cout << "Result: ";
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+}
 int main() {
-	HANDLE extremum_hThread;
-	DWORD extremum_IDThread;
+	HANDLE min_max_hThread;
+	DWORD min_max_IDThread;
 
 	HANDLE average_hThread;
 	DWORD average_IDThread;
+
+	InitializeCriticalSection(&cs);
+	
+	cinArr();
+
+	min_max_hThread = CreateThread(NULL, 0, min_max, NULL, 0, &min_max_IDThread);
+	average_hThread = CreateThread(NULL, 0, average, NULL, 0, &average_IDThread);
+
+	WaitForSingleObject(min_max_hThread, INFINITE);
+	WaitForSingleObject(average_hThread, INFINITE);
+
+	swapExtremum();
+	coutArr();
 }
 
